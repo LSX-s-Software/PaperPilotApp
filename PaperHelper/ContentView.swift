95e26d1db3d707204e347f1d289e2944e65b8164
LSX-s-Software <2682191498@qq.com>
2023-09-24 10:51:11 +0800

    Initial Commit

---
+++ PaperHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaperHelper
+//
+//  Created by 林思行 on 2023/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaperHelper/PaperHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperHelperApp.swift
+//  PaperHelper
+//
+//  Created by 林思行 on 2023/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
