47354dfe1fb82408deba260c9f7f0d01746ef788
LSX-s-Software <2682191498@qq.com>
2023-10-20 18:24:41 +0800

    refactor: AsyncButton使用环境的ControlSize自动设置ProgressView大小

--- PaperPilot/Views/Components/AsyncButton.swift
+++ PaperPilot/Views/Components/AsyncButton.swift
@@ -8,37 +8,40 @@
 import SwiftUI
 
 struct AsyncButton<Content: View>: View {
+    @Environment(\.controlSize) private var controlSize
+
     let title: () -> Content
     let role: ButtonRole?
-    let controlSize: ControlSize
+    var progressSize: ControlSize {
+        switch controlSize {
+        case .mini, .small:
+                .mini
+        case .regular, .large, .extraLarge:
+                .small
+        @unknown default:
+                .regular
+        }
+    }
     let disabled: Bool
     let action: () async -> Void
     
     @State private var loading = false
     
     init(role: ButtonRole? = nil,
-         controlSize: ControlSize = .mini,
          disabled: Bool = false,
          action: @escaping () async -> Void,
          @ViewBuilder label: @escaping () -> Content) {
         self.title = label
         self.role = role
-        self.controlSize = controlSize
         self.disabled = disabled
         self.action = action
     }
     
     init(_ title: LocalizedStringKey,
          role: ButtonRole? = nil,
-         controlSize: ControlSize = .mini,
          disabled: Bool = false,
          action: @escaping () async -> Void) where Content == Text {
-        self.init(
-            role: role,
-            controlSize: controlSize,
-            disabled: disabled,
-            action: action
-        ) {
+        self.init(role: role, disabled: disabled, action: action) {
             Text(title)
         }
     }
@@ -57,7 +60,7 @@ struct AsyncButton<Content: View>: View {
         .overlay {
             if loading {
                 ProgressView()
-                    .controlSize(controlSize)
+                    .controlSize(progressSize)
             }
         }
         .disabled(loading || disabled)
@@ -65,8 +68,31 @@ struct AsyncButton<Content: View>: View {
 }
 
 #Preview {
-    AsyncButton("Submit") {
-        try? await Task.sleep(for: .seconds(1))
+    VStack {
+        AsyncButton("Mini") {
+            try? await Task.sleep(for: .seconds(1))
+        }
+        .controlSize(.mini)
+
+        AsyncButton("Small") {
+            try? await Task.sleep(for: .seconds(1))
+        }
+        .controlSize(.small)
+
+        AsyncButton("Regular") {
+            try? await Task.sleep(for: .seconds(1))
+        }
+        .controlSize(.regular)
+
+        AsyncButton("Large") {
+            try? await Task.sleep(for: .seconds(1))
+        }
+        .controlSize(.large)
+
+        AsyncButton("Extra Large") {
+            try? await Task.sleep(for: .seconds(1))
+        }
+        .controlSize(.extraLarge)
     }
     .padding()
 }
