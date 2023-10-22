c6b4520e0c04a4e40b69665eb4ece76d70a85c3a
LSX-s-Software <2682191498@qq.com>
2023-10-22 21:07:44 +0800

    refactor: 抽离分享逻辑

--- PaperPilot/Views/Projects/ProjectDetail.swift
+++ PaperPilot/Views/Projects/ProjectDetail.swift
@@ -23,8 +23,6 @@ struct ProjectDetail: View {
     @State private var isShowingEditProjectSheet = false
     @State private var isShowingAddPaperSheet = false
     @State private var isShowingSharePopover = false
-    @AppStorage(AppStorageKey.User.username.rawValue)
-    private var username: String?
 
     var onDelete: (() -> Void)?
 
@@ -113,57 +111,7 @@ struct ProjectDetail: View {
                     isShowingSharePopover.toggle()
                 }
                 .popover(isPresented: $isShowingSharePopover, arrowEdge: .bottom) {
-                    VStack {
-                        Image(systemName: "person.3.fill")
-                            .symbolRenderingMode(.hierarchical)
-                            .font(.title)
-                            .foregroundStyle(Color.accentColor)
-                        Text("Invite Others")
-                            .font(.title2)
-                            .fontWeight(.medium)
-
-                        if project.remoteId != nil {
-                            Group {
-                                VStack(alignment: .leading, spacing: 0) {
-                                    Text("Invitation Code")
-                                        .font(.caption)
-                                    HStack {
-                                        TextField("Invitation Code", text: .constant(project.invitationCode ?? ""))
-                                            .textFieldStyle(.roundedBorder)
-                                            .disabled(true)
-                                            .frame(minWidth: 100)
-                                        Button("Copy") {
-                                            if let inviteCode = project.invitationCode {
-                                                setPasteboard(inviteCode)
-                                            }
-                                        }
-                                    }
-                                }
-                                ShareLink(
-                                    "Send Invitation",
-                                    item: URL(string: "https://paperpilot.ziqiang.net.cn/invite.html?invitation=\(project.invitationCode ?? "")")!,
-                                    subject: Text(project.name),
-                                    message: Text("\(username ?? String(localized: "I")) invites you to join the project \"\(project.name)\" on Paper Pilot.")
-                                )
-                            }
-                            .disabled(project.invitationCode == nil || project.invitationCode!.isEmpty)
-                        } else {
-                            HStack {
-                                Image(systemName: "exclamationmark.triangle.fill")
-                                Text("Cannot invite others to join local projects.")
-                            }
-                            .foregroundStyle(.red)
-
-                            AsyncButton("Convert to remote project") {
-                                do {
-                                    try await project.upload()
-                                } catch {
-                                    print(error)
-                                }
-                            }
-                        }
-                    }
-                    .padding()
+                    ShareProjectView(project: project)
                 }
 
                 Button("Project Settings", systemImage: "folder.badge.gear") {
