fa0c22b2903edc5032d902955873b4409795c599
LSX-s-Software <2682191498@qq.com>
2023-11-12 21:35:01 +0800

    update: 优化论文详情栏布局

--- PaperPilot/Views/Reader/Inspector/PaperReaderInspector.swift
+++ PaperPilot/Views/Reader/Inspector/PaperReaderInspector.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-private enum EditableContent {
-    case none
-    case title
-    case author
-}
-
 private enum InspectorContent: String, Identifiable, CaseIterable {
     case info
     case note
@@ -38,102 +32,16 @@ struct PaperReaderInspector: View {
     @AppStorage(AppStorageKey.Reader.isShowingInspector.rawValue)
     private var isShowingInspector = true
 
-    @State private var isShowingEditButton = EditableContent.none
-    @State private var editing = EditableContent.none
-    @State private var newTitle = ""
-    @State private var newAuthor = ""
-    @State private var newAuthors = [String]()
-    @State private var hasError = false
-    @State private var errorMsg: String?
-
     var body: some View {
-        VStack(alignment: .leading) {
-            VStack(alignment: .leading) {
-                Group {
-                    Text(paper.title)
-                        .font(.title)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .overlay(alignment: .trailing) {
-                            if isShowingEditButton == .title {
-                                Button("Edit", systemImage: "pencil") {
-                                    newTitle = paper.title
-                                    editing = .title
-                                }
-                                .labelStyle(.iconOnly)
-                            }
-                        }
-                        .onHover { hover in
-                            isShowingEditButton = hover ? .title : .none
-                        }
-                        .popover(
-                            isPresented: Binding { editing == .title } set: { _ in editing = .none },
-                            arrowEdge: .bottom
-                        ) {
-                            TextField("Enter title", text: $newTitle)
-                                .padding()
-                                .onSubmit {
-                                    if !newTitle.isEmpty {
-                                        handleModifyPaper(newTitle: newTitle)
-                                    }
-                                }
-                        }
-
-                    Text(paper.formattedAuthors)
-                        .foregroundStyle(.secondary)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .overlay(alignment: .trailing) {
-                            if isShowingEditButton == .author {
-                                Button("Edit", systemImage: "pencil") {
-                                    newAuthors = paper.authors
-                                    editing = .author
-                                }
-                                .labelStyle(.iconOnly)
-                            }
-                        }
-                        .onHover { hover in
-                            isShowingEditButton = hover ? .author : .none
-                        }
-                        .popover(
-                            isPresented: Binding { editing == .author } set: { _ in editing = .none },
-                            arrowEdge: .bottom
-                        ) {
-                            List {
-                                ForEach(newAuthors, id: \.self) { author in
-                                    Text(author)
-                                }
-                                .onMove { source, destination in
-                                    newAuthors.move(fromOffsets: source, toOffset: destination)
-                                    handleModifyPaper(newAuthors: newAuthors)
-                                }
-                                .onDelete {
-                                    newAuthors.remove(atOffsets: $0)
-                                    handleModifyPaper(newAuthors: newAuthors)
-                                }
-                                Section("Add author") {
-                                    TextField("New author", text: $newAuthor)
-                                    Button("Add") {
-                                        if !newAuthor.isEmpty {
-                                            newAuthors.append(newAuthor)
-                                            newAuthor = ""
-                                            handleModifyPaper(newAuthors: newAuthors)
-                                        }
-                                    }
-                                    .keyboardShortcut(.defaultAction)
-                                }
-                            }
-                        }
+        VStack(alignment: .leading, spacing: 0) {
+            Picker("Sidebar Content", selection: $inspectorContent) {
+                ForEach(InspectorContent.allCases) { content in
+                    Text(content.localizedStringKey).tag(content)
                 }
-                .multilineTextAlignment(.leading)
-
-                Picker("Sidebar Content", selection: $inspectorContent) {
-                    ForEach(InspectorContent.allCases) { content in
-                        Text(content.localizedStringKey).tag(content)
-                    }
-                }
-                .pickerStyle(.segmented)
-                .labelsHidden()
             }
-            .padding([.horizontal, .top])
+            .pickerStyle(.segmented)
+            .labelsHidden()
+            .padding()
 
             switch inspectorContent {
             case .info:
@@ -161,25 +69,12 @@ struct PaperReaderInspector: View {
             }
 #endif
         }
-        .alert("Failed to update paper info", isPresented: $hasError) { } message: {
-            Text(errorMsg ?? String(localized: "Unknown error"))
-        }
-    }
-
-    func handleModifyPaper(newTitle: String? = nil, newAuthors: [String]? = nil) {
-        if newTitle == nil && newAuthors == nil { return }
-        Task {
-            do {
-                try await ModelService.shared.updatePaper(paper, title: newTitle, authors: newAuthors)
-                editing = .none
-            } catch {
-                hasError = true
-                errorMsg = error.localizedDescription
-            }
-        }
     }
 }
 
 #Preview {
     PaperReaderInspector(paper: ModelData.paper1)
+        .environmentObject(PDFViewModel())
+        .environment(TranslatorViewModel())
+        .frame(width: 300)
 }
