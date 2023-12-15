bb7a6105d7e6f4dcca23179fb6b200b58852d966
LSX-s-Software <2682191498@qq.com>
2023-12-15 20:11:13 +0800

    feat: 支持在添加论文时使用BibTeX补充信息

--- PaperPilot/Views/Projects/NewPaperInfoView.swift
+++ PaperPilot/Views/Projects/NewPaperInfoView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import GRPC
 import OSLog
+import SwiftyBibtex
 
 private let logger = LoggerFactory.make(category: "NewPaperInfoView")
 
@@ -17,6 +18,8 @@ struct NewPaperInfoView: View {
     @Binding var shouldClose: Bool
     @State private var hasError = false
     @State private var errorMsg: String?
+    @State private var bibtex = ""
+    @State private var bibtexParsingError: String?
 
     var body: some View {
         NavigationStack {
@@ -40,8 +43,22 @@ struct NewPaperInfoView: View {
                     TextField("Volume", text: Binding { paper.volume ?? "" } set: { paper.volume = $0.isEmpty ? nil : $0 })
                     TextField("Issue", text: Binding { paper.issue ?? "" } set: { paper.issue = $0.isEmpty ? nil : $0 })
                     TextField("Pages", text: Binding { paper.pages ?? "" } set: { paper.pages = $0.isEmpty ? nil : $0 })
+                    TextField("URL", text: Binding { paper.url ?? "" } set: { paper.url = $0.isEmpty ? nil : $0 })
                     TextField("DOI", text: Binding { paper.doi ?? "" } set: { paper.doi = $0.isEmpty ? nil : $0 })
-                    TextField("Abstract", text: Binding { paper.abstract ?? "" } set: { paper.abstract = $0.isEmpty ? nil : $0 })
+                }
+
+                Section("Or Update By BibTeX") {
+                    TextEditor(text: $bibtex)
+                        .font(.system(.body, design: .monospaced))
+                        .scrollContentBackground(.hidden)
+                        .frame(minWidth: 450, minHeight: 150)
+                    HStack {
+                        Button("Update", action: updatePaperByBibTeX)
+                        if let errorMsg = bibtexParsingError {
+                            Label(errorMsg, systemImage: "exclamationmark.triangle.fill")
+                                .foregroundStyle(.red)
+                        }
+                    }
                 }
             } onDismiss: {
                 // rollback
@@ -85,6 +102,39 @@ struct NewPaperInfoView: View {
                     .disabled(paper.title.isEmpty)
                 }
             }
+        }
+    }
+
+    func updatePaperByBibTeX() {
+        do {
+            let result = try SwiftyBibtex.parse(bibtex)
+            guard let publication = result.publications.first else {
+                bibtexParsingError = String(localized: "No publication found.")
+                return
+            }
+            switch publication {
+            case let article as Article:
+                paper.title = article.title
+                paper.authors = article.author
+                    .split(separator: " and ")
+                    .map { $0.split(separator: ", ").reversed().joined(separator: " ") }
+                paper.publication = article.journal
+                paper.publicationYear = String(format: "%d", article.year)
+                paper.pages = article.pages
+            case let inproceedings as InProceedings:
+                paper.title = inproceedings.title
+                paper.authors = inproceedings.author
+                    .split(separator: " and ")
+                    .map { $0.split(separator: ", ").reversed().joined(separator: " ") }
+                paper.publication = inproceedings.bookTitle
+                paper.publicationYear = String(format: "%d", inproceedings.year)
+                paper.pages = inproceedings.pages
+                if let volume = inproceedings.volume { paper.volume = String(volume) }
+            default:
+                bibtexParsingError = String(localized: "Unsupported publication type.")
+            }
+        } catch {
+            bibtexParsingError = error.localizedDescription
         }
     }
 }
